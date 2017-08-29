169665ba7e4a1b1479309309b95621bf3fcbca41
RhysLiu <bozliu@outlook.com>
2017-08-29 14:10:09 +0800

    main funcation

--- mydll.cpp
+++ mydll.cpp
@@ -3,6 +3,9 @@
 
 #include "stdafx.h"
 #include "mydll.h"
+#include <string>
+#include <iostream>
+using namespace std;
 //for cpp
  int  add(int a, int b)
 {
@@ -27,8 +30,25 @@ EXTERN_C  int myadd(lua_State * L)
 	 lua_pushnumber(L, sum);
 	 return 1;
  }
+EXTERN_C  int mysub(lua_State * L)
+{
+	int result = lua_tonumber(L, 1) - lua_tonumber(L, 2);
+	lua_pushnumber(L, result);
+	return 1;
+}
+EXTERN_C  int str(lua_State * L)
+{
+	string first = lua_tostring(L, 1);
+	string second = lua_tostring(L,2);
+	cout << "hello,I'm in C++" << endl;
+	string s = first + ", " + second;
+	lua_pushstring(L, s.c_str());
+	return 1;
+}
  static luaL_Reg cMethods[] = {
-	 { "myadd", myadd },
+	 { "add", myadd },
+	 {"sub",mysub},
+	 {"string",str},
 	 { NULL, NULL }
  };
  extern "C" __declspec(dllexport) int luaopen_mydll(lua_State* L)
